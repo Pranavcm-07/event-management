741bdf82d6479f1bf43f023063026a85d660eadc
pranav <pranavcm602@gmail.com>
2024-07-05 17:15:51 +0530

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="mt-40 flex justify-center items-center hover:scale-110 transition duration-150">
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:grid-cols-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 3000+ mentors in world-class companies with our global community.
+            </p>
+            <Button size='lg' asChild className="button w-full sm:w-fit">
+              <Link href='/'>
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+          <Image src='/assets/images/hero.png' width={1000} height={1000} alt='Hero Image' className="object-center max-h-[70vh] object-contain 2xl:max-h-[50vh]"/>
+        </div>
+      </section>
+      <section id="events" className="wrapper flex flex-col gap-8 md:gap-12 my-8">
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
+        </h2>
+        <div className="flex flex col md:flex-row gap-5 w-full">
+          Search 
+          CategoryFilter 
+        </div>
+      </section>
+    </>
   );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center flex-between flex-col sm:flex-row text-center flex gap-4 p-5 wrapper'>
+        <Link href='/'>
+          <Image 
+            src='/assets/images/logo.svg' 
+            alt='Evently logo' 
+            width={128} 
+            height={38}
+          />
+        </Link>
+        <p>2023 Evently. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
